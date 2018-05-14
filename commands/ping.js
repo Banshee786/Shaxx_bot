105689e3e8534c4e167dac8a49b22d04229b9049
Banshee786 <kylematthewwilliams@gmail.com>
2018-05-13 20:46:55 -0600

    revert back to v1 system

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name:`Ping`,
-	descrption:`Ping!`,
-	execute(msg, args) {
-		msg.channel.send(`***Pong***`);
-	},
-};
--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const fs = require(`fs`);
+
 const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync(`./commands`);
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -22,37 +14,49 @@ client.on('message', msg => {
   // console.log(msg.content);
 
   //V1 Below
-/*   if (msg.content === `${prefix}ping`) {
+  if (msg.content === `${prefix}ping`) {
     msg.channel.send(`***pong***`);
-  } else if (msg.content === `${prefix}doubles`) {
+ 
+ } else if (msg.content === `${prefix}doubles`) {
      msg.channel.send(`This week's crucible game mode is **doubles**. Grab a partner and prove your strength, guardians!`);
-  } else if (msg.content === `${prefix}factionwar`) {
-     msg.channel.send(`**Faction War is back!** Pledge to your favorite faction and prove your worth to them!`);
-  } else if (msg.content === `${prefix}rumble`) {
-    msg.channel.send(`This week's crucible game mode is **rumble**. Compete against five other guaridans and claim victory!`);
-  } else if (msg.content === `${prefix}mayhem`) {
-    msg.channel.send(`This week's crucible game mode is **mayhem**! Let the chaos escalate. Rise out of the ashes and claim victory, guadians!`);
-  } else if (msg.content === `${prefix}sword`) {
-    msg.channel.send(`https://www.youtube.com/watch?v=RI5KQ_yQLoU`);
-  } else if (msg.content === `${prefix}ibclash`) {
-    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is clash. Prove your worth to the Iron Lords and claim victory!`);
-  } else if (msg.content === `${prefix}ibcontrol`) {
-    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and comepte against another team of six. The game is control. Prove your worth to the Iron Lords and claim victory!`);
-  } else if (msg.content === `${prefix}ibsupremacy`) {
-    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is supremacy. Prove your worth to the Iron Lords and claim victory!`);
-  }  */
-	
-	//V2 before command files
-	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-	
-	const args = msg.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
 
-    if (command === 'ping') {
-        msg.channel.send('Pong.');
-    }
-    
+ } else if (msg.content === `${prefix}factionwar`) {
+     msg.channel.send(`**Faction War is back!** Pledge to your favorite faction and prove your worth to them!`);
+
+ } else if (msg.content === `${prefix}rumble`) {
+    msg.channel.send(`This week's crucible game mode is **rumble**. Compete against five other guaridans and claim victory!`);
+ 
+ } else if (msg.content === `${prefix}mayhem`) {
+    msg.channel.send(`This week's crucible game mode is **mayhem**! Let the chaos escalate. Rise out of the ashes and claim victory, guadians!`);
+
+ } else if (msg.content === `${prefix}sword`) {
+    msg.channel.send(`https://www.youtube.com/watch?v=RI5KQ_yQLoU`);
+ 
+ } else if (msg.content === `${prefix}ibclash`) {
+    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is clash. Prove your worth to the Iron Lords and claim victory!`);
+
+ } else if (msg.content === `${prefix}ibcontrol`) {
+    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and comepte against another team of six. The game is control. Prove your worth to the Iron Lords and claim victory!`);
+ 
+ } else if (msg.content === `${prefix}ibsupremacy`) {
+    msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is supremacy. Prove your worth to the Iron Lords and claim victory!`);
+
+ } 
 	
+	var quotes = [
+		`Fight forever guardian!`,
+		`Stop it! You're SCARING THEM!`,
+		`Was that all of them? THAT WAS ALL OF THEM?`,
+	];
+	
+	if (msg.mentions.includes(client.user)) {
+		msg.channel.send(
+			quotes[
+				Math.floor(Math.random() * quotes.length)
+			]
+		);
+	}
+
 	
 });
 
