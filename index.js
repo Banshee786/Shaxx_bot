a6d4e80640c09ce2c50886a61fb42e1f4b4d1e50
Kevin Williams <kevwil@Kevins-iMac.local>
2018-05-13 21:33:53 -0600

    troubleshooting mentions

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ client.on('message', msg => {
   // debugging
   // console.log(msg.content);
 
+  // ignore other bots
+  if (msg.author.bot) {
+    console.log('Ignoring msg from other bot: ' + msg.author.id);
+    return;
+  }
+
+  // ignore messages that don't start with our Prefix
+  // ***
+  // not working when mentioned
+  // ***
+  // console.log('index of prefix: ' + msg.content.indexOf(prefix));
+  // if (msg.content.indexOf(prefix) !== 0) return;
+
   //V1 Below
   if (msg.content === `${prefix}ping`) {
     msg.channel.send(`***pong***`);
- 
+
  } else if (msg.content === `${prefix}doubles`) {
      msg.channel.send(`This week's crucible game mode is **doubles**. Grab a partner and prove your strength, guardians!`);
 
@@ -25,39 +38,41 @@ client.on('message', msg => {
 
  } else if (msg.content === `${prefix}rumble`) {
     msg.channel.send(`This week's crucible game mode is **rumble**. Compete against five other guaridans and claim victory!`);
- 
+
  } else if (msg.content === `${prefix}mayhem`) {
     msg.channel.send(`This week's crucible game mode is **mayhem**! Let the chaos escalate. Rise out of the ashes and claim victory, guadians!`);
 
  } else if (msg.content === `${prefix}sword`) {
     msg.channel.send(`https://www.youtube.com/watch?v=RI5KQ_yQLoU`);
- 
+
  } else if (msg.content === `${prefix}ibclash`) {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is clash. Prove your worth to the Iron Lords and claim victory!`);
 
  } else if (msg.content === `${prefix}ibcontrol`) {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and comepte against another team of six. The game is control. Prove your worth to the Iron Lords and claim victory!`);
- 
+
  } else if (msg.content === `${prefix}ibsupremacy`) {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is supremacy. Prove your worth to the Iron Lords and claim victory!`);
 
- } 
-	
-	var quotes = [
+ }
+
+	let quotes = [
 		`Fight forever guardian!`,
 		`Stop it! You're SCARING THEM!`,
 		`Was that all of them? THAT WAS ALL OF THEM?`,
 	];
-	
-	if (msg.mentions.includes(client.user)) {
-		msg.channel.send(
-			quotes[
-				Math.floor(Math.random() * quotes.length)
-			]
-		);
-	}
+  var rand = Math.floor(Math.random() * quotes.length);
+  console.log('random quote (' + rand + '): ' + quotes[rand]);
 
-	
+  console.log('mentions has an array of users? ' + Array.isArray(msg.mentions.members));
+  console.log('mentions has an array of users? ' + Array.isArray(msg.mentions));
+  console.log('bot user: ' + client.user);
+
+	// if (msg.mentions.members.includes(client.user)) {
+	// 	msg.channel.send(quotes[rand]);
+	// }
+
+
 });
 
 client.login(token);
