8d4176c462e54e56fbc8ffa8e993e20eed93a8a1
banshee786 <kylematthewwilliams@gmail.com>
2018-07-22 18:12:01 -0600

    ported to legitimate environment instead of AWS, Voice now functional

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const { prefix, token } = require('./config.json');
+// const { exec } = require('child_process');
+const ytdl = require('ytdl-core');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -21,24 +23,52 @@ client.on('message', msg => {
     console.log('Ignoring msg from other bot: ' + msg.author.username + '#' + msg.author.discriminator);
     return;
   }
-  
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
   //ingores messages not in guilds(servers)
-    // if (!msg.guild) return;
-    
-    // //MUSIC
-    
-    // if (msg.content === `${prefix}join`) {
-        
-    //     if (msg.member.voiceChannel) {
-    //         msg.member.voiceChannel.join()
-    //         .then(connection => {
-    //             msg.reply('Are you ready, Oryx slayer?');
-    //         })
-    //         .catch(console.log);
-    //     } else {
-    //         msg.reply("No holding back! Your enemies won't!");
-    //     }
-    // }
+    if (!msg.guild) return;
+
+    //MUSIC
+
+    if (command === 'join') {
+
+        if (msg.member.voiceChannel) {
+            msg.member.voiceChannel.join()
+            .then(connection => {
+                msg.reply('Are you ready, Oryx slayer?');
+            })
+            .catch(console.log);
+        } else {
+            msg.reply("No holding back! Your enemies won't!");
+        }
+    }
+    if (command === 'leave') {
+
+        if (msg.member.voiceChannel) {
+            msg.member.voiceChannel.leave();
+        }
+    }
+    if (command === 'play') {
+      if (args.length < 1) {
+        msg.reply(`syntax is important, guardian: ${prefix}play [url]`);
+        return;
+      }
+      if (msg.member.voiceChannel) {
+        console.log('getting url for audio stream...');
+        // args[0] is youtube url to play
+        const streamOptions = { seek: 0, volume: 0.5 };
+        const stream = ytdl(`${args[0]}`, { filter : 'audioonly' });
+        msg.member.voiceChannel.connection.playStream(stream, streamOptions);
+      }
+    }
+    if (command === 'stop') {
+      if (msg.member.voiceChannel) {
+          console.log('stopping music.');
+          msg.member.voiceChannel.connection.dispatcher.end();
+      }
+    }
   // ignore messages that don't start with our Prefix
   // ***
   // not working when mentioned
@@ -47,34 +77,34 @@ client.on('message', msg => {
   // if (msg.content.indexOf(prefix) !== 0) return;
 
   //V1 Below
-  if (msg.content === `${prefix}ping`) {
+  if (command === 'ping') {
     msg.channel.send(`***pong***`);
 
- } else if (msg.content === `${prefix}doubles`) {
+ } else if (command === 'doubles') {
      msg.channel.send(`This week's crucible game mode is **doubles**. Grab a partner and prove your strength, guardians!`);
 
- } else if (msg.content === `${prefix}factionwar`) {
+ } else if (command === 'factionwar') {
      msg.channel.send(`**Faction War is back!** Pledge to your favorite faction and prove your worth to them!`);
 
- } else if (msg.content === `${prefix}rumble`) {
+ } else if (command === 'rumble') {
     msg.channel.send(`This week's crucible game mode is **rumble**. Compete against five other guaridans and claim victory!`);
 
- } else if (msg.content === `${prefix}mayhem`) {
+ } else if (command === 'mayhem') {
     msg.channel.send(`This week's crucible game mode is **mayhem**! Let the chaos escalate. Rise out of the ashes and claim victory, guadians!`);
 
- } else if (msg.content === `${prefix}sword`) {
+ } else if (command === 'sword') {
     msg.channel.send(`https://www.youtube.com/watch?v=RI5KQ_yQLoU`);
 
-} else if (msg.content === `${prefix}ibclash`) {
+} else if (command === 'ibclash') {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is clash. Prove your worth to the Iron Lords and claim victory!`);
 
- } else if (msg.content === `${prefix}ibcontrol`) {
+ } else if (command === 'ibcontrol') {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and comepte against another team of six. The game is control. Prove your worth to the Iron Lords and claim victory!`);
 
- } else if (msg.content === `${prefix}ibsupremacy`) {
+ } else if (command === 'ibsupremacy') {
     msg.channel.send(`Lord Saladin is back. **Iron Banner is live!** Gather a fireteam and compete against another team of six. The game is supremacy. Prove your worth to the Iron Lords and claim victory!`);
 
- } else if (msg.content === `${prefix}shaxxHelp`) {
+ } else if (command === 'shaxxHelp') {
  	msg.channel.send(`!ping \n!doubles \n!factionwar \n!rumble \n!mayhem \n!sword \n!ibclash \n!ibcontrol \n!ibsupremacy`);
  }
 
@@ -125,9 +155,9 @@ client.on('message', msg => {
   if (msg.isMentioned(client.user)) {
     msg.channel.send(quotes[rand]);
   }
-  
-  
-  
+
+
+
 });
 
 client.login(token);
